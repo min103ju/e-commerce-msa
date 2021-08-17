22a5a194d6f9bc979ef1c57b9a5a28582a0b59d2
Min Heo <min103ju@gmail.com>
2021-08-18 04:02:35 +0900

    Init Commit

---
+++ discoveryservice/src/main/java/com/citizen/discoveryservice/DiscoveryserviceApplication.java
@@ -0,0 +1,16 @@
+package com.citizen.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// TODO: 2021-08-18 Eureka Server임을 설정
+@EnableEurekaServer
+public class DiscoveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryserviceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/main/java/com/citizen/userservice/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.citizen.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+// TODO: 2021-08-18 Eureka Client임을 설정 
+// TODO: 2021-08-18 @EnableDiscoveryClient와 EnableEurekaClient의 차이 확인  
+@EnableDiscoveryClient
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
