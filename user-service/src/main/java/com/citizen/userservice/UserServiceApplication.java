9f2b2c534fb5d21b33519c75dc435ba56c2a7535
Min Heo <min103ju@gmail.com>
2021-08-20 08:03:24 +0900

    feat(gateway): spring cloud gateway service project 생성

--- user-service/src/main/java/com/citizen/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/citizen/userservice/UserServiceApplication.java
@@ -3,10 +3,11 @@ package com.citizen.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 // TODO: 2021-08-18 Eureka Client임을 설정 
-// TODO: 2021-08-18 @EnableDiscoveryClient와 EnableEurekaClient의 차이 확인  
+// TODO: 2021-08-18 @EnableDiscoveryClient와 EnableEurekaClient의 차이 확인
 @EnableDiscoveryClient
 public class UserServiceApplication {
 
