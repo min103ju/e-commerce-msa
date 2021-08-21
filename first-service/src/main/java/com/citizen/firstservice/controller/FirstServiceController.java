8cea04e4d628ee03e26a2d6994286f9c58a2c061
Min Heo <min103ju@gmail.com>
2021-08-22 00:11:18 +0900

    feat(load balancer): Load Balancer 설ì정 부분 추가

--- first-service/src/main/java/com/citizen/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/citizen/firstservice/controller/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.citizen.firstservice.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @RestController
 public class FirstServiceController {
+
+    // TODO: 2021-08-22 application.yml에 정의된 속성 정보를 조회하는 객체
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,8 +32,16 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on Port %s.",
+            // TODO: 2021-08-22 application.yml에 random port를 조회
+            // TODO: 2021-08-22 실제 할당되어진 port
+            env.getProperty("local.server.port"));
+
+        // 52924
+
+        // 52944
     }
 
 }
